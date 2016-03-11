cd907192f65d8d4c8ca611e493cfadda1137584b
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-11 14:19:33 +1100

    update ResultItem and test

--- src/components/SearchResults/ResultItem.js
+++ src/components/SearchResults/ResultItem.js
@@ -1,10 +1,14 @@
 
 import React from 'react';
+import Logo from '../../components/SearchResults/Logo';
 import FlightDetails from '../../components/SearchResults/FlightDetails';
 import Price from '../../components/SearchResults/Price';
+import SelectButton from '../../components/SearchResults/SelectButton';
 
 export default class ResultItem extends React.Component {
   static propTypes = {
+    _id: React.PropTypes.string.isRequired,
+    airline: React.PropTypes.object.isRequired,
     flight: React.PropTypes.object.isRequired,
     return: React.PropTypes.object,
     price: React.PropTypes.object.isRequired,
@@ -12,11 +16,16 @@ export default class ResultItem extends React.Component {
   };
   render = () => {
     return (
-      <div className={`search-result-item ${ this.props.return ? 'return' : 'one-way'}`}>
+      <div className={`search-result-item ${ this.props.return ? 'return' : 'one-way' }`}>
+        <Logo imageUri={this.props.airline.logoUri} />
         <FlightDetails {...this.props.flight} />
         { !!this.props.return ? (<FlightDetails {...this.props.return} />) : (undefined) }
         <Price {...this.props.price} />
+        <SelectButton onSelect={this.onSelectHandler} />
       </div>
     );
+  };
+  onSelectHandler = () => {
+    this.props.onSelect(this.props._id);
   };
 }
--- test/specs/components/SearchResults/ResultItem.js
+++ test/specs/components/SearchResults/ResultItem.js
@@ -5,85 +5,134 @@ import sinon from 'sinon';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
+import Logo from '../../../../src/components/SearchResults/Logo';
 import FlightDetails from '../../../../src/components/SearchResults/FlightDetails';
+import Price from '../../../../src/components/SearchResults/Price';
+import SelectButton from '../../../../src/components/SearchResults/SelectButton';
 import ResultItem from '../../../../src/components/SearchResults/ResultItem';
 
 describe('Search Result Item Component', () => {
 
-  it('should show the one way flight details', () => {
-    const TEST_FLIGHT_DETAILS = {
-      number: 'AI-202',
-      from: 'PNQ',
-      destination: 'DEL',
-      depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
-      arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+  it('should show airline logo, price, select button, and departure flight details', () => {
+    const RESULT_ITEN_PROPS = {
+      _id: 'TEST_ID',
+      airline: { logoUri: 'LOGO_URI' },
+      flight: {
+        number: 'AI-202',
+        from: 'PNQ',
+        destination: 'DEL',
+        depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
+        arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+      },
+      price: { symbol: '£', value: 125 },
+      onSelect: sinon.spy()
     };
-    const TEST_PRICE = { symbol: '£', value: 125 };
-    const TEST_ON_SELECT_HANDLER = sinon.spy();
-    let wrapper = shallow(<ResultItem flight={TEST_FLIGHT_DETAILS}
-      price={TEST_PRICE} onSelect={TEST_ON_SELECT_HANDLER} />);
-    expect(wrapper.contains(<FlightDetails {...TEST_FLIGHT_DETAILS} />)).to.eql(true);
+    let wrapper = shallow(<ResultItem {...RESULT_ITEN_PROPS} />);
+    expect(wrapper.contains(<Logo imageUri={RESULT_ITEN_PROPS.airline.logoUri} />)).to.eql(true);
+    expect(wrapper.contains(<FlightDetails {...RESULT_ITEN_PROPS.flight} />)).to.eql(true);
+    expect(wrapper.contains(<Price {...RESULT_ITEN_PROPS.price} />)).to.eql(true);
+    expect(wrapper.contains(<SelectButton onSelect={wrapper.instance().onSelectHandler} />)).to.eql(true);
   });
 
-  it('should show the return flight details', () => {
-    const TEST_FLIGHT_DETAILS = {
-      number: 'AI-202',
-      from: 'PNQ',
-      destination: 'DEL',
-      depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
-      arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+  it('should show logo, price, select button, and the departure and the return flight details', () => {
+    const RESULT_ITEN_PROPS = {
+      _id: 'TEST_ID',
+      airline: { logoUri: 'LOGO_URI' },
+      flight: {
+        number: 'AI-202',
+        from: 'PNQ',
+        destination: 'DEL',
+        depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
+        arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+      },
+      return: {
+        number: 'AI-203',
+        from: 'PNQ',
+        destination: 'DEL',
+        depart: moment('2014-01-10 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
+        arrive: moment('2014-01-10 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+      },
+      price: { symbol: '£', value: 125 },
+      onSelect: sinon.spy()
     };
-    const TEST_RETURN_FLIGHT_DETAILS = {
-      number: 'AI-203',
-      from: 'PNQ',
-      destination: 'DEL',
-      depart: moment('2014-01-10 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
-      arrive: moment('2014-01-10 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
-    };
-    const TEST_PRICE = { symbol: '£', value: 125 };
-    const TEST_ON_SELECT_HANDLER = sinon.spy();
-    let wrapper = shallow(<ResultItem flight={TEST_FLIGHT_DETAILS} return={TEST_RETURN_FLIGHT_DETAILS}
-      price={TEST_PRICE} onSelect={TEST_ON_SELECT_HANDLER} />);
-    expect(wrapper.contains(<FlightDetails {...TEST_FLIGHT_DETAILS} />)).to.eql(true);
-    expect(wrapper.contains(<FlightDetails {...TEST_RETURN_FLIGHT_DETAILS} />)).to.eql(true);
+    let wrapper = shallow(<ResultItem {...RESULT_ITEN_PROPS} />);
+    expect(wrapper.contains(<Logo imageUri={RESULT_ITEN_PROPS.airline.logoUri} />)).to.eql(true);
+    expect(wrapper.contains(<FlightDetails {...RESULT_ITEN_PROPS.flight} />)).to.eql(true);
+    expect(wrapper.contains(<FlightDetails {...RESULT_ITEN_PROPS.return} />)).to.eql(true);
+    expect(wrapper.contains(<Price {...RESULT_ITEN_PROPS.price} />)).to.eql(true);
+    expect(wrapper.contains(<SelectButton onSelect={wrapper.instance().onSelectHandler} />)).to.eql(true);
   });
 
   it('should have the search-result-item and one-way class name', () => {
-    const TEST_FLIGHT_DETAILS = {
-      number: 'AI-202',
-      from: 'PNQ',
-      destination: 'DEL',
-      depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
-      arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+    const RESULT_ITEN_PROPS = {
+      _id: 'TEST_ID',
+      airline: { logoUri: 'LOGO_URI' },
+      flight: {
+        number: 'AI-202',
+        from: 'PNQ',
+        destination: 'DEL',
+        depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
+        arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+      },
+      price: { symbol: '£', value: 125 },
+      onSelect: sinon.spy()
     };
-    const TEST_PRICE = { symbol: '£', value: 125 };
-    const TEST_ON_SELECT_HANDLER = sinon.spy();
-    let wrapper = shallow(<ResultItem flight={TEST_FLIGHT_DETAILS}
-      price={TEST_PRICE} onSelect={TEST_ON_SELECT_HANDLER} />);
+    let wrapper = shallow(<ResultItem {...RESULT_ITEN_PROPS} />);
     expect(wrapper.hasClass('search-result-item')).to.eql(true);
     expect(wrapper.hasClass('one-way')).to.eql(true);
   });
 
   it('should have the search-result-item and return class name', () => {
-    const TEST_FLIGHT_DETAILS = {
-      number: 'AI-202',
-      from: 'PNQ',
-      destination: 'DEL',
-      depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
-      arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+    const RESULT_ITEN_PROPS = {
+      _id: 'TEST_ID',
+      airline: { logoUri: 'LOGO_URI' },
+      flight: {
+        number: 'AI-202',
+        from: 'PNQ',
+        destination: 'DEL',
+        depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
+        arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+      },
+      return: {
+        number: 'AI-203',
+        from: 'PNQ',
+        destination: 'DEL',
+        depart: moment('2014-01-10 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
+        arrive: moment('2014-01-10 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+      },
+      price: { symbol: '£', value: 125 },
+      onSelect: sinon.spy()
     };
-    const TEST_RETURN_FLIGHT_DETAILS = {
-      number: 'AI-203',
-      from: 'PNQ',
-      destination: 'DEL',
-      depart: moment('2014-01-10 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
-      arrive: moment('2014-01-10 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
-    };
-    const TEST_PRICE = { symbol: '£', value: 125 };
-    const TEST_ON_SELECT_HANDLER = sinon.spy();
-    let wrapper = shallow(<ResultItem flight={TEST_FLIGHT_DETAILS} return={TEST_RETURN_FLIGHT_DETAILS}
-      price={TEST_PRICE} onSelect={TEST_ON_SELECT_HANDLER} />);
+    let wrapper = shallow(<ResultItem {...RESULT_ITEN_PROPS} />);
     expect(wrapper.hasClass('search-result-item')).to.eql(true);
     expect(wrapper.hasClass('return')).to.eql(true);
+  });
+
+  it('should trigger onSelect handler and provide _id', () => {
+    const RESULT_ITEN_PROPS = {
+      _id: 'TEST_ID',
+      airline: { logoUri: 'LOGO_URI' },
+      flight: {
+        number: 'AI-202',
+        from: 'PNQ',
+        destination: 'DEL',
+        depart: moment('2014-01-01 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
+        arrive: moment('2014-01-01 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+      },
+      return: {
+        number: 'AI-203',
+        from: 'PNQ',
+        destination: 'DEL',
+        depart: moment('2014-01-10 10:00 AM', 'YYYY-MM-DD HH:mm A').valueOf(),
+        arrive: moment('2014-01-10 12:00 PM', 'YYYY-MM-DD HH:mm A').valueOf(),
+      },
+      price: { symbol: '£', value: 125 },
+      onSelect: sinon.spy()
+    };
+    let wrapper = shallow(<ResultItem {...RESULT_ITEN_PROPS} />);
+    expect(wrapper.contains(<SelectButton onSelect={wrapper.instance().onSelectHandler} />)).to.eql(true);
+    wrapper.instance().onSelectHandler();
+    expect(RESULT_ITEN_PROPS.onSelect.called).to.eql(true);
+    expect(RESULT_ITEN_PROPS.onSelect.getCall(0).args[0]).to.eql(RESULT_ITEN_PROPS._id);
   });
 });
