f5e8458e929ff4efd12e0dbf4cc280b949d3f313
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-13 23:13:02 +1100

    fixed test of price slider component

--- test/specs/components/Apps/FlightSearchApp.js
+++ test/specs/components/Apps/FlightSearchApp.js
@@ -26,4 +26,71 @@ describe('FlightSearchApp Component', () => {
       expect(wrapper.hasClass('flight-search-app')).to.eql(true);
     });
   });
+
+  // describe('componentDidMount function', () => {
+  //
+  //   it('should call subscribeResultStore function', () => {
+  //     let wrapper = shallow(<PriceSlider />);
+  //     wrapper.instance().subscribeResultStore = sinon.spy();
+  //     wrapper.instance().componentDidMount();
+  //     expect(wrapper.instance().subscribeResultStore.called).to.eql(true);
+  //   });
+  // });
+  //
+  // describe('componentWillUnmount function', () => {
+  //
+  //   it('should call unsubscribeResultStore function', () => {
+  //     let wrapper = shallow(<PriceSlider />);
+  //     wrapper.instance().unsubscribeResultStore = sinon.spy();
+  //     wrapper.instance().componentWillUnmount();
+  //     expect(wrapper.instance().unsubscribeResultStore.called).to.eql(true);
+  //   });
+  // });
+  //
+  // describe('getAdjustedMaxValue function', () => {
+  //
+  //   it('should return an interger larger then the max price of tickets', () => {
+  //     const TEST_RESULT = genSearchResults(ticketTypes.RETURN, 10);
+  //     let wrapper = shallow(<PriceSlider />);
+  //     let result = wrapper.instance().getAdjustedMaxValue(TEST_RESULT.tickets);
+  //     _.forEach(TEST_RESULT.tickets, ticket => expect(ticket.price.value <= result));
+  //   });
+  // });
+
+  // describe('onResultStoreChange function', () => {
+  //
+  //   it('should update state', () => {
+  //     const TEST_RESULT = genSearchResults(ticketTypes.RETURN, 10);
+  //     const MAX_PRICE = 200;
+  //     let wrapper = shallow(<PriceSlider />);
+  //     wrapper.instance().setState = sinon.spy();
+  //     wrapper.instance().getAdjustedMaxValue = sinon.stub().returns(MAX_PRICE);
+  //     wrapper.instance().onResultStoreChange(TEST_RESULT);
+  //     expect(wrapper.instance().getAdjustedMaxValue.called).to.eql(true);
+  //     expect(wrapper.instance().getAdjustedMaxValue.getCall(0).args[0]).to.eql(TEST_RESULT.tickets);
+  //     expect(wrapper.instance().setState.called).to.eql(true);
+  //     expect(wrapper.instance().setState.getCall(0).args[0]).to.eql({
+  //       hidden: false,
+  //       max: MAX_PRICE,
+  //       symbol: '£',
+  //       values: [ 0, MAX_PRICE ]
+  //     });
+  //   });
+  // });
+
+  // describe('subscribeResultStore function', () => {
+  //
+  //   it('should subscribe to result store', () => {
+  //     const TEST_RESULT = genSearchResults(ticketTypes.RETURN, 10);
+  //     let wrapper = shallow(<PriceSlider />);
+  //     wrapper.instance().onResultStoreChange = sinon.spy();
+  //     wrapper.instance().subscribeResultStore();
+  //     resultStore.dispatch({ type: 'UPDATE', result: TEST_RESULT });
+  //
+  //     expect(wrapper.instance().unsubscribeResultStore).to.not.eql(undefined);
+  //     expect(wrapper.instance().onResultStoreChange.called).to.eql(true);
+  //     expect(wrapper.instance().onResultStoreChange.getCall(0).args[0]).to.eql(TEST_RESULT);
+  //     wrapper.instance().unsubscribeResultStore();
+  //   });
+  // });
 });
--- test/specs/components/PriceSlider/index.js
+++ test/specs/components/PriceSlider/index.js
@@ -16,144 +16,63 @@ import { genSearchResults } from '../../../utils/gen';
 
 describe('PriceSlider Component', () => {
 
-  describe('initial state', () => {
-
-    it('should have the min, max, values, symbol, hidden state', () => {
-      const STATE = {
-        min: 0,
-        max: 10,
-        hidden: true,
-        values: [ 0, 10 ],
-        symbol: ''
-      };
-      let wrapper = shallow(<PriceSlider />);
-      expect(wrapper.instance().state).to.eql(STATE);
-    });
-  });
-
   describe('render function', () => {
 
     it('should render title', () => {
-      let wrapper = shallow(<PriceSlider />);
+      const PROPS = {
+        onFilter: sinon.spy(),
+        max: 0,
+        symbol: '',
+        values: [ 0, 0 ]
+      };
+      let wrapper = shallow(<PriceSlider {...PROPS} />);
       expect(wrapper.contains(<h5>refine flight search</h5>)).to.eql(true);
     });
 
     it('should render labels', () => {
-      const DEFAULT_STATE = {
-        min: 0,
-        max: 10,
-        hidden: false,
-        values: [ 0, 10 ],
-        symbol: ''
+      const PROPS = {
+        onFilter: sinon.spy(),
+        max: 0,
+        symbol: '',
+        values: [ 0, 0 ]
       };
-      let wrapper = shallow(<PriceSlider />);
-      expect(wrapper.contains(<label className="min-price">{ `${DEFAULT_STATE.symbol}${DEFAULT_STATE.min}` }</label>)).to.eql(true);
-      expect(wrapper.contains(<label className="max-price">{ `${DEFAULT_STATE.symbol}${DEFAULT_STATE.max}` }</label>)).to.eql(true);
+      let wrapper = shallow(<PriceSlider {...PROPS} />);
+      expect(wrapper.contains(<label className="min-price">{ `${PROPS.symbol}${0}` }</label>)).to.eql(true);
+      expect(wrapper.contains(<label className="max-price">{ `${PROPS.symbol}${PROPS.max}` }</label>)).to.eql(true);
     });
 
     it('should render react slider', () => {
-      const DEFAULT_STATE = {
-        min: 0,
-        max: 10,
-        hidden: false,
-        values: [ 0, 10 ],
-        symbol: ''
+      const PROPS = {
+        onFilter: sinon.spy(),
+        max: 0,
+        symbol: '',
+        values: [ 0, 0 ]
       };
-      let wrapper = shallow(<PriceSlider />);
+      let wrapper = shallow(<PriceSlider {...PROPS} />);
       expect(wrapper.find(ReactSlider)).to.have.length(1);
-      expect(wrapper.find(ReactSlider).prop('min')).to.eql(DEFAULT_STATE.min);
-      expect(wrapper.find(ReactSlider).prop('max')).to.eql(DEFAULT_STATE.max);
-      expect(wrapper.find(ReactSlider).prop('value')).to.eql(DEFAULT_STATE.values);
+      expect(wrapper.find(ReactSlider).prop('min')).to.eql(0);
+      expect(wrapper.find(ReactSlider).prop('max')).to.eql(PROPS.max);
+      expect(wrapper.find(ReactSlider).prop('value')).to.eql(PROPS.values);
       expect(wrapper.find(ReactSlider).prop('onChange')).to.eql(wrapper.instance().onChangeHandler);
-      expect(wrapper.find(ReactSlider).contains(<div className="custom-handle">{ DEFAULT_STATE.values[0] }</div>)).to.eql(true);
-      expect(wrapper.find(ReactSlider).contains(<div className="custom-handle">{ DEFAULT_STATE.values[1] }</div>)).to.eql(true);
-    });
-  });
-
-  describe('componentDidMount function', () => {
-
-    it('should call subscribeResultStore function', () => {
-      let wrapper = shallow(<PriceSlider />);
-      wrapper.instance().subscribeResultStore = sinon.spy();
-      wrapper.instance().componentDidMount();
-      expect(wrapper.instance().subscribeResultStore.called).to.eql(true);
-    });
-  });
-
-  describe('componentWillUnmount function', () => {
-
-    it('should call unsubscribeResultStore function', () => {
-      let wrapper = shallow(<PriceSlider />);
-      wrapper.instance().unsubscribeResultStore = sinon.spy();
-      wrapper.instance().componentWillUnmount();
-      expect(wrapper.instance().unsubscribeResultStore.called).to.eql(true);
-    });
-  });
-
-  describe('subscribeResultStore function', () => {
-
-    it('should subscribe to result store', () => {
-      const TEST_RESULT = genSearchResults(ticketTypes.RETURN, 10);
-      let wrapper = shallow(<PriceSlider />);
-      wrapper.instance().onResultStoreChange = sinon.spy();
-      wrapper.instance().subscribeResultStore();
-      resultStore.dispatch({ type: 'UPDATE', result: TEST_RESULT });
-
-      expect(wrapper.instance().unsubscribeResultStore).to.not.eql(undefined);
-      expect(wrapper.instance().onResultStoreChange.called).to.eql(true);
-      expect(wrapper.instance().onResultStoreChange.getCall(0).args[0]).to.eql(TEST_RESULT);
-      wrapper.instance().unsubscribeResultStore();
-    });
-  });
-
-  describe('onResultStoreChange function', () => {
-
-    it('should update state', () => {
-      const TEST_RESULT = genSearchResults(ticketTypes.RETURN, 10);
-      const MAX_PRICE = 200;
-      let wrapper = shallow(<PriceSlider />);
-      wrapper.instance().setState = sinon.spy();
-      wrapper.instance().getAdjustedMaxValue = sinon.stub().returns(MAX_PRICE);
-      wrapper.instance().onResultStoreChange(TEST_RESULT);
-      expect(wrapper.instance().getAdjustedMaxValue.called).to.eql(true);
-      expect(wrapper.instance().getAdjustedMaxValue.getCall(0).args[0]).to.eql(TEST_RESULT.tickets);
-      expect(wrapper.instance().setState.called).to.eql(true);
-      expect(wrapper.instance().setState.getCall(0).args[0]).to.eql({
-        hidden: false,
-        max: MAX_PRICE,
-        symbol: '£',
-        values: [ 0, MAX_PRICE ]
-      });
-    });
-  });
-
-  describe('getAdjustedMaxValue function', () => {
-
-    it('should return an interger larger then the max price of tickets', () => {
-      const TEST_RESULT = genSearchResults(ticketTypes.RETURN, 10);
-      let wrapper = shallow(<PriceSlider />);
-      let result = wrapper.instance().getAdjustedMaxValue(TEST_RESULT.tickets);
-      _.forEach(TEST_RESULT.tickets, ticket => expect(ticket.price.value <= result));
+      expect(wrapper.find(ReactSlider).contains(<div className="custom-handle">{ PROPS.values[0] }</div>)).to.eql(true);
+      expect(wrapper.find(ReactSlider).contains(<div className="custom-handle">{ PROPS.values[1] }</div>)).to.eql(true);
     });
   });
 
   describe('onChangeHandler function', () => {
 
-    it('should update state and filter store', () => {
+    it('should call onFilter function', () => {
       const TEST_VALUES = [ 0, 200 ];
-      let wrapper = shallow(<PriceSlider />);
-      wrapper.instance().setState = sinon.spy();
-      let onFilterStoreChange = sinon.spy();
-      let unsubscribe = filterStore.subscribe(() => {
-        let filter = filterStore.getState();
-        onFilterStoreChange(filter);
-      });
+      const PROPS = {
+        onFilter: sinon.spy(),
+        max: 0,
+        symbol: '',
+        values: [ 0, 0 ]
+      };
+      let wrapper = shallow(<PriceSlider {...PROPS} />);
       let result = wrapper.instance().onChangeHandler(TEST_VALUES);
-      expect(wrapper.instance().setState.called).to.eql(true);
-      expect(wrapper.instance().setState.getCall(0).args[0]).to.eql({ values: TEST_VALUES });
-      expect(onFilterStoreChange.called).to.eql(true);
-      expect(onFilterStoreChange.getCall(0).args[0]).to.eql({ min: TEST_VALUES[0], max: TEST_VALUES[1] });
-      unsubscribe();
+      expect(PROPS.onFilter.called).to.eql(true);
+      expect(PROPS.onFilter.getCall(0).args[0]).to.eql({ min: TEST_VALUES[0], max: TEST_VALUES[1] });
     });
   });
 });
