18f464f5d92c0f26eda5b6e9cbe199aaf1e693fa
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-13 11:32:59 +1100

    refactor slider

--- src/components/PriceSlider/index.js
+++ src/components/PriceSlider/index.js
@@ -4,24 +4,28 @@ import ReactSlider from 'react-slider';
 import resultStore from '../../browser/stores/results';
 
 export default class PriceSlider extends React.Component {
-  static propTypes = {
-    min: React.PropTypes.number,
-    max: React.PropTypes.number.isRequired
-  };
-  static defaultProps = {
-    min: 0,
-    max: 0
-  };
   state = {
-    min: this.props.min,
-    max: this.props.max,
-    hidden: true
+    min: 0,
+    max: 10,
+    hidden: true,
+    values: [ 0, 10 ],
+    symbol: ''
   };
   render = () => {
     return (
-      <div className="price-slider">
-        <ReactSlider defaultValue={[ this.props.min, this.props.max ]} withBars={true}
-          onChange={this.onChangeHandler} />
+      <div className={`price-slider ${this.state.hidden ? 'hidden' : 'shown'}`}>
+        <h5>refine flight search</h5>
+        <div className="price-labels">
+          <label className="min-price">{ `${this.state.symbol}${this.state.min}` }</label>
+          <label className="max-price">{ `${this.state.symbol}${this.state.max}` }</label>
+        </div>
+        <ReactSlider min={this.state.min} max={this.state.max}
+          value={this.state.values} withBars={true}
+          minDistance={10} pearling={true}
+          onChange={this.onChangeHandler} >
+          <div className="custom-handle">{ this.state.values[0] }</div>
+          <div className="custom-handle">{ this.state.values[1] }</div>
+        </ReactSlider>
       </div>
     );
   };
@@ -38,10 +42,17 @@ export default class PriceSlider extends React.Component {
     });
   };
   onResultStoreChange = (result = {}) => {
-    let maxPrice = _.maxBy(result.items, item => item.price.value);
-    return this.setState({ hidden: result.items && result.items.length > 0, max: maxPrice && maxPrice.value || 0 });
+    let maxPriceTicket = _.maxBy(result.items, item => item.price.value);
+    let maxPrice = maxPriceTicket && maxPriceTicket.price || undefined;
+    let maxValue = maxPrice && maxPrice.value || 0;
+    return this.setState({
+      hidden: !result.items || result.items.length === 0,
+      max: maxValue,
+      symbol: maxPrice.symbol,
+      values: [ 0, maxValue ]
+    });
   };
-  onChangeHandler = (data) => {
-    console.log(data);
+  onChangeHandler = (values) => {
+    this.setState({ values: values });
   };
 }
