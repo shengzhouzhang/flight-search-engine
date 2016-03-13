dec13995b334710653c17eab1b2c9f58f9535778
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-13 11:39:55 +1100

    add random price

--- src/server/controllers/apis/FlightSearchApi.js
+++ src/server/controllers/apis/FlightSearchApi.js
@@ -20,52 +20,58 @@ export default class FlightSearchApp {
     switch(query.type) {
       case ticketTypes.ONEWAY:
         return res.status(200)
-          .json({
-            query: query,
-            items: _.range(10).map((index) =>
-              new TicketOneWay(
-                `ticket-${index}`,
-                new Airline(''),
-                new Flight(
-                  `departure-flight-${index}`,
-                  'AI-202',
-                  query.from,
-                  query.destination,
-                  moment(query.departureDate, 'YYYY-MM-DD').hour(10).minute(0).startOf('minute').valueOf(),
-                  moment(query.departureDate, 'YYYY-MM-DD').hour(12).minute(0).startOf('minute').valueOf()
-                ),
-                new Price('£', 125 * query.passengers)
-              ))
-            });
+          .json({ query: query, items: genOneWayTickets(10, query) });
       case ticketTypes.RETURN:
         return res.status(200)
-          .json({
-            query: query,
-            items: _.range(10).map((index) =>
-              new TicketReturn(
-                `ticket-${index}`,
-                new Airline(''),
-                new Flight(
-                  `departure-flight-${index}`,
-                  'AI-202',
-                  query.from,
-                  query.destination,
-                  moment(query.departureDate, 'YYYY-MM-DD').hour(10).minute(0).startOf('minute').valueOf(),
-                  moment(query.departureDate, 'YYYY-MM-DD').hour(12).minute(0).startOf('minute').valueOf()
-                ),
-                new Flight(
-                  `return-flight-${index}`,
-                  'AI-203',
-                  query.destination,
-                  query.from,
-                  moment(query.returnDate, 'YYYY-MM-DD').hour(10).minute(0).startOf('minute').valueOf(),
-                  moment(query.returnDate, 'YYYY-MM-DD').hour(12).minute(0).startOf('minute').valueOf()
-                ),
-                new Price('£', 125 * query.passengers)
-              ))
-            });
+          .json({ query: query, items: genReturnTickets(10, query) });
         default:
           return res.status(400).json({ message: `invalid ticket type: ${query.type}`});
     }
   };
 };
+
+export function genOneWayTickets (size, query) {
+  return _.range(size)
+    .map((index) =>
+      new TicketOneWay(
+        `ticket-${index}`,
+        new Airline(''),
+        new Flight(
+          `departure-flight-${index}`,
+          'AI-202',
+          query.from,
+          query.destination,
+          moment(query.departureDate, 'YYYY-MM-DD').hour(10).minute(0).startOf('minute').valueOf(),
+          moment(query.departureDate, 'YYYY-MM-DD').hour(12).minute(0).startOf('minute').valueOf()
+        ),
+        new Price('£', _.random(50, 200) * query.passengers)
+      )
+    );
+}
+
+export function genReturnTickets (size, query) {
+  return _.range(size)
+    .map((index) =>
+      new TicketReturn(
+        `ticket-${index}`,
+        new Airline(''),
+        new Flight(
+          `departure-flight-${index}`,
+          'AI-202',
+          query.from,
+          query.destination,
+          moment(query.departureDate, 'YYYY-MM-DD').hour(10).minute(0).startOf('minute').valueOf(),
+          moment(query.departureDate, 'YYYY-MM-DD').hour(12).minute(0).startOf('minute').valueOf()
+        ),
+        new Flight(
+          `return-flight-${index}`,
+          'AI-203',
+          query.destination,
+          query.from,
+          moment(query.returnDate, 'YYYY-MM-DD').hour(10).minute(0).startOf('minute').valueOf(),
+          moment(query.returnDate, 'YYYY-MM-DD').hour(12).minute(0).startOf('minute').valueOf()
+        ),
+        new Price('£', _.random(50, 200) * query.passengers)
+    )
+  );
+}
