cd5b20b26d47dd4f03014d36c12efab3f6f37634
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-14 00:47:44 +1100

    add currency domain test

--- src/domains/Currency.js
+++ src/domains/Currency.js
@@ -3,12 +3,6 @@ import _ from 'lodash';
 import currencyTypes from '../config/currencyTypes';
 
 export default class Currency {
-  static fromCode = (code) => {
-    let currency = _.find(currencyTypes, currency => currency.code === code);
-    if (!currency) { throw new Error(`invalud currency code: ${code}`); }
-    console.log(currency);
-    return new Currency(currency.code, currency.symbol);
-  };
   static fromJson = (raw = {}) => {
     return new Currency(raw.code, raw.symbol);
   };
---
+++ test/specs/domains/Currency.js
@@ -0,0 +1,26 @@
+
+import { expect } from 'chai';
+import currencyTypes from '../../../src/config/currencyTypes';
+import Currency from '../../../src/domains/Currency';
+
+describe('Currency Domain', () => {
+
+  describe('constructor', () => {
+
+    it('should set code and symbol', () => {
+      const CURRENCY = currencyTypes.GBP;
+      let currency = new Currency(CURRENCY.code, CURRENCY.symbol);
+      expect(currency.code).to.eql(CURRENCY.code);
+      expect(currency.symbol).to.eql(CURRENCY.symbol);
+    });
+  });
+
+  describe('fromJson function', () => {
+
+    it('should construct an Airline from Json', () => {
+      const RAW = currencyTypes.GBP;
+      let currency = Currency.fromJson(RAW);
+      expect(currency).to.eql(new Currency(RAW.code, RAW.symbol));
+    });
+  });
+});
