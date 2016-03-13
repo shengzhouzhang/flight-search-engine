b722b114d081bac62a7b9b56cca9b87b4de88314
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-14 00:44:14 +1100

    add airline domain test

---
+++ test/specs/domains/Airline.js
@@ -0,0 +1,25 @@
+
+import { expect } from 'chai';
+import Airline from '../../../src/domains/Airline';
+
+describe('Airline Domain', () => {
+
+  describe('constructor', () => {
+
+    it('should set logo image uri', () => {
+      const IMAGE_URI = 'IMAGE_URI';
+      let airline = new Airline(IMAGE_URI);
+      expect(airline.logo.imageUri).to.eql(IMAGE_URI);
+    });
+  });
+
+  describe('fromJson function', () => {
+
+    it('should construct an Airline from Json', () => {
+      const IMAGE_URI = 'IMAGE_URI';
+      const RAW = { logo: { imageUri: IMAGE_URI }};
+      let airline = Airline.fromJson(RAW);
+      expect(airline).to.eql(new Airline(IMAGE_URI));
+    });
+  });
+});
